15b3a2bfe97bb67c9677a825ed662909e89a4d5d
SUHAIL AKRAM MOHAMMED <suhailakram93@gmail.com>
2020-07-02 18:50:50 -0400

    Delete Funtionality added

---
+++ BookListRazor/Controllers/BookController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookListRazor.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookListRazor.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public BookController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult GetAll()
+        {
+            return Json(new {data = _db.Book.ToList() });
+        }
+    }
+}
