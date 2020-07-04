dcf530aa9d1ffad9e2996aa7703619b4fcf5f5f8
SUHAIL AKRAM MOHAMMED <suhailakram93@gmail.com>
2020-07-03 20:49:33 -0400

    Delete Functionality (JS & Ajax)

--- BookListRazor/Controllers/BookController.cs
+++ BookListRazor/Controllers/BookController.cs
@@ -4,9 +4,13 @@ using System.Linq;
 using System.Threading.Tasks;
 using BookListRazor.Model;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.Web.CodeGeneration.Contracts.Messaging;
 
 namespace BookListRazor.Controllers
 {
+    [Route("api/Book")]
+    [ApiController]
     public class BookController : Controller
     {
         private readonly ApplicationDbContext _db;
@@ -15,9 +19,23 @@ namespace BookListRazor.Controllers
         {
             _db = db;
         }
-        public IActionResult GetAll()
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
         {
-            return Json(new {data = _db.Book.ToList() });
+            return Json(new {data = await _db.Book.ToListAsync() });
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var bookFromDb = await _db.Book.FirstOrDefaultAsync(u => u.Id == id);
+            if (bookFromDb == null)
+            {
+                return Json(new { success = false, message = "Error while deleting" });
+            }
+            _db.Book.Remove(bookFromDb);
+            await _db.SaveChangesAsync();
+            return Json(new { success = true, message = "Deleted Sucessfully" });
         }
     }
 }
